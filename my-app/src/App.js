759c30791c6a0445330d772792b82076812d896b
Danyil Gregul <daniel.gregul95@gmail.com>
2020-10-16 00:33:10 +0200

    Basics Learned!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -9,12 +10,28 @@ class App extends Component {
       { name: 'Max', age: 78, belt: 'white', id:3 }    
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja =>{
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
-        <Ninjas ninjas={this.state.ninjas} /> {/*imported from Ninjas.js*/}
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} /> {/*imported from Ninjas.js*/}
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,23 +1,35 @@
 import React from 'react'; // no class = no need to import {Component}
 
-const Ninjas = ({ninjas}) => { // no class => no render method. this is a function/ ui component
+const Ninjas = ({ninjas, deleteNinja}) => { // no class => no render method. this is a function/ ui component
   // in functional comps props need to be passed down via () unlike class based where its attached to the instance of the comp
   // state = {} doesn't work here
   // const { ninjas } = props; not needed, because function parameter ({ninjas}) is based of of props
+  // const ninjaList = ninjas.map(ninja => {
+  //   if (ninja.age > 30) {  // if statement
+  //   return (
+  //       <div className="ninja" key={ninja.id}> {/*key lässt react instanzen von ninja genau zuordnen */}
+  //         <div>Name: {ninja.name}</div> {/* this.props.name not necessery due to being stored in name in line 6 */}
+  //         <div>Age: {ninja.age}</div>
+  //         <div>Belt: {ninja.belt}</div>
+  //       </div>
+  //     )
+  //   }
+  // })
   const ninjaList = ninjas.map(ninja => {
-    return (
+    return ninja.age > 20 ? ( // ternary operator. syntax: condition ? (true) : (false)
       <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div> {/* this.props.name not necessery due to being stored in name in line 6 */}
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
+        <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>  {/*Arrow anonymisiert function damit (ninja.id) nicht automatisch ausgelöst wird */}
       </div>
-    )
+    ) : null; 
   })
-  return (
+  return ( // ternary operator could also be placed instead ninjaList
     <div className="ninja-list">
       { ninjaList }
     </div>
-    )
+  )
 }
 
 export default Ninjas;
